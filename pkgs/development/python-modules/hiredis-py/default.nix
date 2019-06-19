1434bbd06e42d561725362748647361a74e82095
BadDecisionsAlex <alex.ameen.tx@gmail.com>
2019-06-19 02:42:53 -0500

    python3Packages.hiredis-py: init at 1.0.0

---
+++ pkgs/development/python-modules/hiredis-py/default.nix
@@ -0,0 +1,23 @@
+{ lib
+, fetchPypi
+, buildPythonPackage
+}:
+buildPythonPackage rec {
+
+  pname = "hiredis-py";
+  version = "1.0.0";
+
+  src = fetchurl {
+    inherit pname version;
+    sha256 = "e97c953f08729900a5e740f1760305434d62db9f281ac351108d6c4b5bf51795";
+  };
+
+  doCheck = false;
+
+  meta = with lib; {
+    homepage = "http://github.com/redis/hiredis-py";
+    license = licenses.bsdOriginal;
+    description = "Python extension that wraps protocol parsing code in hiredis. It primarily speeds up parsing of multi bulk replies.";
+  };
+
+}
