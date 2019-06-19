5cec9380344f8a535c96bd9cdab9485c3a59faff
BadDecisionsAlex <alex.ameen.tx@gmail.com>
2019-06-19 03:39:07 -0500

    fetchPypi -> fetchurl

--- pkgs/development/python-modules/hiredis-py/default.nix
+++ pkgs/development/python-modules/hiredis-py/default.nix
@@ -8,8 +8,8 @@ buildPythonPackage rec {
   version = "1.0.0";
 
   src = fetchurl {
-    sha256 = "e97c953f08729900a5e740f1760305434d62db9f281ac351108d6c4b5bf51795";
     url = "https://files.pythonhosted.org/packages/9e/e0/c160dbdff032ffe68e4b3c576cba3db22d8ceffc9513ae63368296d1bcc8/hiredis-1.0.0.tar.gz";
+    sha256 = "e97c953f08729900a5e740f1760305434d62db9f281ac351108d6c4b5bf51795";
   };
 
   doCheck = false;
