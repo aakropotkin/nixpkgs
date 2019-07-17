e417f80e752f8504b611847c2923517165da69f3
BadDecisionsAlex <alex.ameen.tx@gmail.com>
2019-07-16 23:10:50 -0500

    rolled back cachix

--- pkgs/development/tools/cachix/default.nix
+++ pkgs/development/tools/cachix/default.nix
@@ -1,8 +1,3 @@
 { haskellPackages, haskell }:
 
-haskell.lib.justStaticExecutables (haskellPackages.override {
-  overrides = self: super: {
-    cachix = haskell.lib.doDistribute (self.cachix_0_2_1 or self.cachix);
-    cachix-api = self.cachix-api_0_2_1 or self.cachix-api;
-  };
-}).cachix
+(haskell.lib.doDistribute haskellPackages.cachix).bin
